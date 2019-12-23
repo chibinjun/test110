27d13567d690d6c4f013816155e6277372a4aaba
chibinjun <742956023@qq.com>
2019-12-23 22:02:27 +0800

    我们完成了第一个功能！

---
+++ page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>php测试</title>
+	</head>
+	<body>
+		<div>welcome to php</div>
+		<?php
+           echo'<div>hello world</div>';
+        ?>
+	</body>
+</html>
